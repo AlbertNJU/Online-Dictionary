79ab298aa1312921707ddd790e4e902b2759bdee
AlbertNJU <524560435@qq.com>
2016-12-19 21:41:23 +0800

    Update dictionaryDB.java

--- dictionaryDB.java
+++ dictionaryDB.java
@@ -1,3 +1,10 @@
+import java.awt.Image;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -5,7 +12,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.imageio.stream.FileImageOutputStream;
 
+import WordCard.WordCard;
+
+//数据库类
 public class dictionaryDB {
 	private String url = "jdbc:mysql://localhost:3306/online-dictionary?characterEncoding=utf8&useSSL=true";
 	private String username = "root";
@@ -14,11 +25,14 @@ public class dictionaryDB {
 	private PreparedStatement pst;
 	private ResultSet rs;
 	private String sql;
+	private Integer card_ID;
 	
+	//初始化，连接数据库
 	public void Init(){
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(url , username , password);
+			card_ID = 0;
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -29,11 +43,7 @@ public class dictionaryDB {
 		}
 	}
 	
-	/*public String searchPassword(String username){
-		String a = new String();
-		return a;
-	}*/
-	
+	//查询某单词点赞数
 	public ArrayList<Integer> searchNumberOfLikes(String wordToBeLookedUp){		
 		ArrayList<Integer> numOfLikes = new ArrayList<Integer>();
 		try {
@@ -63,6 +73,7 @@ public class dictionaryDB {
 		return numOfLikes;
 	}
 	
+	//增加或减少某单词点赞数
 	public void changeNumberOfLikes(String wordToBeLookedUp, int i, int mode){		
 		try {
 			//increse 1
@@ -104,8 +115,7 @@ public class dictionaryDB {
 		}	
 	}
 	
-	
-	
+	//查询某用户密码
 	public String searchPassword(String account){
 		String result = new String();
 		try{
@@ -126,15 +136,15 @@ public class dictionaryDB {
 		return result;
 	}
 	
-	public int searchID(){
+	//查询目前最大的用户ID
+	public int searchUserID(){
 		int result = 0;
 		try {
 			sql = "Select max(ID) from user";
 			pst = conn.prepareStatement(sql);
 			rs = pst.executeQuery();
 			rs.next();
-			String result_str = rs.getString(1);
-			result = Integer.parseInt("1");
+			result = rs.getInt(1);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -142,6 +152,23 @@ public class dictionaryDB {
 		return result;
 	}
 	
+	//查询目前最大的单词卡ID
+	public int searchCardID(){
+		int result = 0;
+		try {
+			sql = "Select max(ID) from wordcard";
+			pst = conn.prepareStatement(sql);
+			rs = pst.executeQuery();
+			rs.next();		
+			result = rs.getInt(1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	//添加用户
 	public String addUser(int ID, String account, String password){
 		String result = new String();
 		try {
@@ -151,12 +178,13 @@ public class dictionaryDB {
 			rs = pst.executeQuery();
 		 
 			if(!rs.next()){
-				sql = "Insert into user(ID, name, password, account) values(?, ?, ?, ?)" ;
+				sql = "Insert into user(ID, name, password, account, online) values(?, ?, ?, ?, ?)" ;
 				pst = conn.prepareStatement(sql);
 				pst.setObject(1, ID);
 				pst.setObject(2, account);
 				pst.setObject(3, password);
 				pst.setObject(4, account);
+				pst.setObject(5, 1);
 				pst.execute();
 				result = "Y";
 			}
@@ -169,12 +197,106 @@ public class dictionaryDB {
 		return result;		
 	}
 	
+	//查询所有在线用户
+	public ArrayList<String> SearchOnlineUsers(){
+		ArrayList<String> result = new ArrayList<String>();
+		try {
+			sql = "SELECT name, account FROM user where online = 1";
+			pst = conn.prepareStatement(sql);
+			rs = pst.executeQuery();
+		 
+			while(rs.next()){
+				String temp = rs.getString(1) + "(" + rs.getString(2) + ")";
+				result.add(temp);
+			}				
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
 	
-	/*public static void main(String args[]){
-		dictionaryDB test = new dictionaryDB();
-		test.Init();
-		ArrayList<Integer> result = test.searchNumberOfLikes("a");
-		System.out.println(result.toArray()[0] + "\t" + result.toArray()[1]+ "\t" + result.toArray()[2]);
-		
-	}*/
+	//设置某用户在线
+	public void setUserOnline(String account){
+		try {
+			sql = "UPDATE user SET online = 1 where account = ?";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, account);
+			pst.execute();		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//设置某用户下线
+	public void setUserOffline(String account){
+		try {
+			sql = "UPDATE user SET online = 0 where account = ?";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, account);
+			pst.execute();		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	// 将图片插入数据库
+	public String addCard(int id_card, WordCard new_card ){
+        String result = new String();
+		try { 
+            sql = "Insert into wordcard(ID, from_account, to_account, card, word) values(?, ?, ?, ?, ?)";    
+            pst = conn.prepareStatement(sql);
+            pst.setInt(1, id_card);   
+            pst.setString(2, new_card.from_account);
+            pst.setString(3, new_card.to_account);
+            pst.setObject(4, new_card.picture);
+            pst.setString(5, new_card.word);
+           
+            boolean success = pst.execute();
+            if(success == true)
+            	result = "Y";
+            else
+            	result = "N";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+		return result;
+    }
+
+    // 读取数据库中图片
+    public ArrayList<WordCard> searchCards(String to_id) {
+    	ArrayList<WordCard> cards = new ArrayList<WordCard>();
+        try {
+            String sql = "select * from wordcard where to_account = ?";
+            pst = conn.prepareStatement(sql);
+            pst.setObject(1, to_id);
+            rs = pst.executeQuery();
+            WordCard card = new WordCard();
+            while (rs.next()) {
+            	card = new WordCard();
+            	InputStream in = rs.getBinaryStream("card");
+                ByteArrayOutputStream output = new ByteArrayOutputStream();  
+                int len = 0;
+                byte[] buf = new byte[1024];
+                while ((len = in.read(buf)) != -1) {
+                    output.write(buf, 0, len);
+                }
+                card.picture = output.toByteArray();	
+                card.from_account = rs.getString(2);
+                card.to_account = to_id;
+                card.word = rs.getString(5);
+                //System.out.println(card.word);
+                cards.add(card);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+        for(int i = 0; i < cards.size(); i++){
+			System.out.println(cards.get(i).word);
+		}
+        return cards;
+    }
 }
